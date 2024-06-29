9e3f5b72d13f6d7d32598d8c005b8bea35b2dae5
JamesTJenkins <jenkins01tj@gmail.com>
2024-06-29 17:58:40 +0100

    Fixed inconsistent whitespaces issue

--- mist/include/Core.hpp
+++ mist/include/Core.hpp
@@ -2,30 +2,30 @@
 #include <memory>
 
 #ifdef MIST_DLL
-    #define MIST_API __declspec(dllexport)
+	#define MIST_API __declspec(dllexport)
 #else
-    #define MIST_API __declspec(dllimport)
+	#define MIST_API __declspec(dllimport)
 #endif
 
 #if _WIN32
 #elif __linux__
-    #error "Linux not supported yet"
+	#error "Linux not supported yet"
 #else
-    #error "Unsupported Platform"
+	#error "Unsupported Platform"
 #endif
 
 namespace mist {
-    template<typename T>
-    using Scope = std::unique_ptr<T>;
-    template<typename T, typename ... Args>
-    constexpr Scope<T> CreateScope(Args&& ... args) {
-        return std::make_unique<T>(std::forward<Args>(args)...);
-    }
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+	template<typename T, typename ... Args>
+	constexpr Scope<T> CreateScope(Args&& ... args) {
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
 
-    template<typename T>
-    using Ref = std::shared_ptr<T>;
-    template<typename T, typename ... Args>
-    constexpr Ref<T> CreateRef(Args&& ... args) {
-        return std::make_shared<T>(std::forward<Args>(args)...);
-    }
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+	template<typename T, typename ... Args>
+	constexpr Ref<T> CreateRef(Args&& ... args) {
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
 }
