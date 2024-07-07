973c1625b0e2b7d3109acf463fc9e0e194b95415
JamesTJenkins <jenkins01tj@gmail.com>
2024-07-07 17:07:21 +0100

    Added some windows utils for use in editor

---
+++ mist/src/platform/WindowsPlatformUtils.cpp
@@ -0,0 +1,52 @@
+#include "PlatformUtils.hpp"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <commdlg.h>
+#include <SDL.h>
+#include <SDL_syswm.h>
+#include "Application.hpp"
+
+namespace mist {
+	static HWND GetWindowsWindow() {
+		SDL_SysWMinfo wmInfo;
+		SDL_VERSION(&wmInfo.version);
+		SDL_GetWindowWMInfo((SDL_Window*)Application::Get().GetWindow().GetNativeWindow(), &wmInfo);
+		return wmInfo.info.win.window;
+	}
+
+	std::string FileDialog::OpenFile(const char* filter) {
+		OPENFILENAMEA ofn;	// Common dialog box structure
+		CHAR szFile[260] = { 0 };
+		ZeroMemory(&ofn, sizeof(OPENFILENAME));
+		ofn.lStructSize = sizeof(OPENFILENAME);
+		ofn.hwndOwner = GetWindowsWindow();
+		ofn.lpstrFile = szFile;
+		ofn.nMaxFile = sizeof(szFile);
+		ofn.lpstrFilter = filter;
+		ofn.nFilterIndex = 1;
+		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+		if (GetOpenFileNameA(&ofn) == TRUE) {
+			return ofn.lpstrFile;
+		}
+		return std::string();
+	}
+
+	std::string FileDialog::SaveFile(const char* filter) {
+		OPENFILENAMEA ofn;	// Common dialog box structure
+		CHAR szFile[260] = { 0 };
+		ZeroMemory(&ofn, sizeof(OPENFILENAME));
+		ofn.lStructSize = sizeof(OPENFILENAME);
+		ofn.hwndOwner = GetWindowsWindow();
+		ofn.lpstrFile = szFile;
+		ofn.nMaxFile = sizeof(szFile);
+		ofn.lpstrFilter = filter;
+		ofn.nFilterIndex = 1;
+		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+		if (GetOpenFileNameA(&ofn) == TRUE) {
+			return ofn.lpstrFile;
+		}
+		return std::string();
+	}
+}
