c4cc4e5bad3db499b2bc6af3adc9728d0574ddb8
JamesTJenkins <jenkins01tj@gmail.com>
2024-07-07 23:21:52 +0100

    Updated run loop

--- mist/src/Application.cpp
+++ mist/src/Application.cpp
@@ -5,49 +5,69 @@
 #include "renderer/RenderCommand.hpp"
 
 namespace mist {
-    Application* Application::instance{};
+	Application* Application::instance{};
 
-    Application::Application(const char* name) {
-        if (instance != nullptr)
-            throw std::runtime_error("Created 2 application instances");
+	Application::Application(const char* name) {
+		if (instance != nullptr)
+			throw std::runtime_error("Created 2 application instances");
 
-        instance = this;
-        SDL_Init(SDL_INIT_VIDEO);
+		instance = this;
+		SDL_Init(SDL_INIT_VIDEO);
 
-        window = std::unique_ptr<Window>(Window::Create(WindowProperties(name)));
+		window = std::unique_ptr<Window>(Window::Create(WindowProperties(name)));
 
-        RenderCommand::Initialize();
-    }
+		RenderCommand::Initialize();
+	}
 
-    Application::~Application() {
-        RenderCommand::Shutdown();
-        SDL_Quit();
-    }
+	Application::~Application() {
+		RenderCommand::Shutdown();
+		SDL_Quit();
+	}
 
-    void Application::Run() {
-        while (running) {
-            SDL_Event event;
+	void Application::Run() {
+		while (running) {
+			SDL_Event event;
 			while (SDL_PollEvent (&event)) {
 				switch (event.type) {
+				case SDL_WINDOWEVENT_RESIZED:
+					RenderCommand::ResizeWindow(window->GetXPosition(), window->GetYPosition(), window->GetWidth(), window->GetHeight());
+					break;
+				case SDL_WINDOWEVENT_CLOSE:
 				case SDL_QUIT:
 					Quit();
 					break;
 				}
+
+				for (Layer* layer : layerStack) {
+					layer->OnEvent(&event);
+				}
 			}
+		}
+
+		for (Layer* layer : layerStack) {
+			layer->OnUpdate();
+		}
+
+		imguiLayer->Begin();
+		for (Layer* layer : layerStack) {
+            layer->OnRender();
         }
-    }
+		imguiLayer->End();
 
-    void Application::Quit() {
-        running = false;
-    }
+		window->OnUpdate();
+	}
 
-    void Application::PushLayer(Layer* layer) {
-        layerStack.PushLayer(layer);
-        layer->OnAttach();
-    }
+	void Application::Quit() {
+		running = false;
+	}
 
-    void Application::PopLayer(Layer* layer) {
-        layer->OnDetach();
-        layerStack.PopLayer(layer);
-    }
+	void Application::PushLayer(Layer* layer) {
+		layerStack.PushLayer(layer);
+		layer->OnAttach();
+	}
+
+	void Application::PopLayer(Layer* layer) {
+		layer->OnDetach();
+		layerStack.PopLayer(layer);
+	}
 }
