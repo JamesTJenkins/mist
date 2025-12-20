afd7c2ff834196095c9edd808c456cf0671d4b8c
JamesTJenkins <jenkins01tj@protonmail.com>
2025-12-20 19:19:07 +0000

    Updated transform to fix issues with rotations

--- mist/include/components/Transform.hpp
+++ mist/include/components/Transform.hpp
@@ -5,7 +5,7 @@
 namespace mist {
     class Transform {
     public:
-        Transform(glm::vec3 position = glm::vec3(0, 0, 0), glm::quat rotation = glm::quat(), glm::vec3 scale = glm::vec3(1, 1, 1));
+        Transform(glm::vec3 position = glm::vec3(0, 0, 0), glm::quat rotation = glm::quat_identity<float, glm::defaultp>(), glm::vec3 scale = glm::vec3(1, 1, 1));
         
         bool IsEqual(const Transform& other) const;
 
--- mist/src/components/Transform.cpp
+++ mist/src/components/Transform.cpp
@@ -2,60 +2,54 @@
 #include <glm/gtx/matrix_decompose.hpp>
 
 namespace mist {
-    Transform::Transform(glm::vec3 position, glm::quat rotation, glm::vec3 scale) {
-        SetPosition(position);
-        SetRotation(rotation);
-        SetScale(scale);
-    }
+	Transform::Transform(glm::vec3 position, glm::quat rotation, glm::vec3 scale) : position(position), rotation(rotation), scale(scale) {}
 
-    bool Transform::IsEqual(const Transform& other) const {
-        return position == other.position &&
-            rotation == other.rotation &&
-            scale == other.scale;
-    }
+	bool Transform::IsEqual(const Transform& other) const {
+		return position == other.position &&
+			rotation == other.rotation &&
+			scale == other.scale;
+	}
 
-    void Transform::Rotate(float angle_in_radians, glm::vec3 axis) {
-        axis *= sin(angle_in_radians / 2.0f);
-        rotation = glm::normalize(glm::quat(cos(angle_in_radians / 2.0f), axis) * rotation);
-    }
+	void Transform::Rotate(float angle_in_radians, glm::vec3 axis) {
+		rotation *= glm::angleAxis(angle_in_radians, glm::normalize(axis));
+	}
 
-    glm::quat Transform::EulerToQuat(glm::vec3 rotation_in_degrees) {
-        return glm::quat(glm::radians(rotation_in_degrees));
-    }
+	glm::quat Transform::EulerToQuat(glm::vec3 rotation_in_degrees) {
+		return glm::quat(glm::radians(rotation_in_degrees));
+	}
 
-    glm::vec3 Transform::QuatToEuler(glm::quat quaternion) {
-        return glm::degrees(glm::eulerAngles(quaternion));
-    }
+	glm::vec3 Transform::QuatToEuler(glm::quat quaternion) {
+		return glm::degrees(glm::eulerAngles(quaternion));
+	}
 
-    void Transform::SetPosition(glm::vec3 position) { this->position = position; }
-    void Transform::SetRotation(glm::quat rotation) { this->rotation = rotation; }
-    void Transform::SetScale(glm::vec3 scale) { this->scale = scale; }
+	void Transform::SetPosition(glm::vec3 position) { this->position = position; }
+	void Transform::SetRotation(glm::quat rotation) { this->rotation = rotation; }
+	void Transform::SetScale(glm::vec3 scale) { this->scale = scale; }
 
-    glm::vec3 Transform::GetPosition() const { return position; }
-    glm::quat Transform::GetRotation() const { return rotation; }
-    glm::vec3 Transform::GetScale() const { return scale; }
+	glm::vec3 Transform::GetPosition() const { return position; }
+	glm::quat Transform::GetRotation() const { return rotation; }
+	glm::vec3 Transform::GetScale() const { return scale; }
 
-    glm::vec3 Transform::Left() const { return rotation * glm::vec4(1.0f, 0.0f, 0.0f, 0.0f); }
-    glm::vec3 Transform::Right() const { return rotation * glm::vec4(-1.0f, 0.0f, 0.0f, 0.0f); }
-    glm::vec3 Transform::Up() const { return rotation * glm::vec4(0.0f, 1.0f, 0.0f, 0.0f); }
-    glm::vec3 Transform::Down() const { return rotation * glm::vec4(0.0f, -1.0f, 0.0f, 0.0f); }
-    glm::vec3 Transform::Forward() const { return rotation * glm::vec4(0.0f, 0.0f, 1.0f, 0.0f); }
-    glm::vec3 Transform::Backward() const { return rotation * glm::vec4(0.0f, 0.0f, -1.0f, 0.0f); }
+	glm::vec3 Transform::Left() const { return rotation * glm::vec3(1.0f, 0.0f, 0.0f); }
+	glm::vec3 Transform::Right() const { return rotation * glm::vec3(-1.0f, 0.0f, 0.0f); }
+	glm::vec3 Transform::Up() const { return rotation * glm::vec3(0.0f, 1.0f, 0.0f); }
+	glm::vec3 Transform::Down() const { return rotation * glm::vec3(0.0f, -1.0f, 0.0f); }
+	glm::vec3 Transform::Forward() const { return rotation * glm::vec3(0.0f, 0.0f, 1.0f); }
+	glm::vec3 Transform::Backward() const { return rotation * glm::vec3(0.0f, 0.0f, -1.0f); }
 
-    glm::mat4 Transform::GetLocalToWorldMatrix() const {
-        glm::mat4 translationMatrix = glm::translate(position);
-        glm::mat4 rotationMatrix = glm::mat4_cast(rotation);
-        glm::mat4 scaleMatrix = glm::scale(scale);
+	glm::mat4 Transform::GetLocalToWorldMatrix() const {
+		glm::mat4 translationMatrix = glm::translate(position);
+		glm::mat4 rotationMatrix = glm::mat4_cast(rotation);
+		glm::mat4 scaleMatrix = glm::scale(scale);
+		
+		return glm::mat4(translationMatrix * rotationMatrix * scaleMatrix);
+	}
 
-        return glm::mat4(translationMatrix * rotationMatrix * scaleMatrix);
-    }
+	glm::mat4 Transform::GetWorldToLocalMatrix() const {
+		glm::mat4 translationMatrix = glm::translate(-position);
+		glm::mat4 rotationMatrix = glm::mat4_cast(glm::inverse(rotation));
+		glm::mat4 scaleMatrix = glm::scale(1.0f / scale);
 
-    glm::mat4 Transform::GetWorldToLocalMatrix() const {
-        glm::mat4 translationMatrix = glm::translate(-position);
-        // glm::mat4 rotationMatrix = glm::mat4_cast(-rotation);
-        glm::mat4 rotationMatrix = glm::mat4_cast(glm::inverse(rotation));
-        glm::mat4 scaleMatrix = glm::scale(1.0f / scale);
-
-        return glm::mat4(scaleMatrix * rotationMatrix * translationMatrix);
-    }
+		return glm::mat4(scaleMatrix * rotationMatrix * translationMatrix);
+	}
 }
